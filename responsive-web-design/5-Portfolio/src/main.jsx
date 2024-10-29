fc97188a19d7e5295381d72252fd0b97de1a0b84
Axel Le Meur <axel.le.meur@outlook.com>
2024-10-29 16:51:49 +0100

    changed js files to jsx + started react rooter

---
+++ responsive-web-design/5-Portfolio/src/main.jsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import'/styles/css/main.css';
