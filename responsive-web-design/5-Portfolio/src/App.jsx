69188e6cda70eda34a40e2771279f3610e82ee8e
Axel Le Meur <axel.le.meur@outlook.com>
2024-10-30 10:33:28 +0100

    Started working on routing

--- responsive-web-design/5-Portfolio/src/App.jsx
+++ responsive-web-design/5-Portfolio/src/App.jsx
@@ -1,2 +1,10 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+
+function App(){
+    return <div className="Body">
+        Yop
+    </div>
+}
+
+export default App;
