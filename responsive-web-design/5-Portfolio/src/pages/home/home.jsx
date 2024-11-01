0e8e608deedb153a43dacb5f5de2eb80e4cf0c18
Axel Le Meur <95547308+fahrenheigt@users.noreply.github.com>
2024-11-01 15:17:30 +0100

    Created home page and home animation

--- responsive-web-design/5-Portfolio/src/pages/home/home.jsx
+++ responsive-web-design/5-Portfolio/src/pages/home/home.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
-import { useState } from 'react';
-import NavBar from '../../components/navBar/navBar';
+import React, { useEffect } from 'react';
+import anime from "animejs";
 
 export default function Home() {
+    useEffect(() => {
+        const container = document.querySelector('.container');
+        
+        // Créer et ajouter les blocs
+        for (var i = 0; i < 100; i++) {
+            const blocks = document.createElement('div');
+            blocks.classList.add('block');
+            container.appendChild(blocks);
+        }
+
+        // Fonction d'animation des blocs
+        function animateBlocks() {
+            anime({
+                targets: '.block',
+                translateX: function () {
+                    return anime.random(-700, 700);
+                },
+                translateY: function () {
+                    return anime.random(-500, 500);
+                },
+                scale: function () {
+                    return anime.random(1,5);
+                },
+                easing: 'easeOutElastic(1, .8)',
+                duration: 3000,
+                delay: anime.stagger(50),
+                complete: animateBlocks,
+            });
+        }
+
+        // Démarrer l'animation
+        animateBlocks();
+    }, []); // Le tableau vide garantit que l'effet ne s'exécute qu'une fois après le premier rendu
 
     return (
-        <div className='body'>
+        <div className='home-container'>
+            <div className='container'>
+                <h2>Axel Le Meur</h2>
+            </div>
         </div>
     );
 }
